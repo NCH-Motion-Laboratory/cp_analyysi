4cc0c89c99bd0bbd8eeb64562cade8aa4c36f8db
Jussi Nurminen <jnu@iki.fi>
2018-07-18 16:51:29 +0300

    autoproc check enhs

--- cp_check_autoproc.py
+++ cp_check_autoproc.py
@@ -2,15 +2,18 @@
 """
 CP, check autoproc results
 
-Load random trials sequentially in Nexus
+Load random trials sequentially in Nexus. Show kinematics. Save evaluation to
+xls
 
 @author: Jussi (jnu@iki.fi)
 """
 
+import sys
 import os.path as op
 from random import shuffle
 
 import gaitutils
+from gaitutils import GaitDataError
 
 from cp_common import write_workbook, get_subjects, get_timestr, get_files
 
@@ -20,7 +23,7 @@ trial_types = ['normal', 'cognitive']
 
 timestr_ = get_timestr()
 # results shall be entered into this workbook
-xls_filename = 'z:/CP_projekti_analyysit/cp_autoproc_check_%s.xlsx' % timestr_
+xls_filename = 'K:/CP_projekti_analyysit/cp_autoproc_check_%s.xlsx' % timestr_
 
 trials = list()
 subjects = get_subjects()
@@ -31,15 +34,29 @@ shuffle(trials)
 print 'Got trials'
 
 trials_ = trials[:N]
-#write_workbook([trials_], xls_filename)
+write_workbook([trials_], xls_filename)
 
 vicon = gaitutils.nexus.viconnexus()
 
+evs = list()
 for trial in trials_:
     trialbase = op.splitext(trial)[0]
+    print 'Opening %s' % trial
     vicon.OpenTrial(trialbase, 200)
-    print 'Opened %s:' % trial
-    desc = gaitutils.eclipse.get_eclipse_keys(trialbase + '.Trial.enf')['DESCRIPTION']
-    print desc
-    print 'Press enter to open next trial'
-    raw_input()
+    print 'Plotting kinematics, close window after review...'
+    sys.stdout.flush()
+    try:
+        gaitutils.nexus_kinallplot.do_plot()
+    except (GaitDataError, ValueError):
+        print 'Failed to plot kinematics'
+    enf = '%s.Trial.enf' % trialbase
+    desc = gaitutils.eclipse.get_eclipse_keys(enf)['DESCRIPTION']
+    print 'Description: %s' % desc
+    print 'Enter evaluation (hit Enter for ok):'
+    sys.stdout.flush()
+    ev = raw_input()
+    if not ev:
+        ev = 'ok'
+    evs.append(ev)
+
+write_workbook([trials_, evs], xls_filename)
