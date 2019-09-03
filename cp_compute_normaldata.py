ebc7bdc9843d9a7de40d477cc9efa405236402e2
Jussi Nurminen <jnu@iki.fi>
2019-09-03 17:46:03 +0300

    wip: cleanup compute_normaldata

--- cp_compute_normaldata.py
+++ cp_compute_normaldata.py
@@ -11,11 +11,46 @@ import os.path as op
 import logging
 import datetime
 
-from gaitutils import analysis, c3d, stats, viz, layouts, normaldata, models
+from gaitutils import analysis, stats, normaldata
 from cp_common import get_files, params, get_subjects
 
 
 logger = logging.getLogger(__name__)
+
+
+# supplementary data: ages for TD subjects
+age_di = {'TD01':	13.1,
+          'TD02':	12.2,
+          'TD03':	12.2,
+          'TD04':	14.3,
+          'TD05':	15.9,
+          'TD07':	11.6,
+          'TD08':	16.7,
+          'TD09':	11.6,
+          'TD10':	12.3,
+          'TD11':	13.8,
+          'TD12':	17.6,
+          'TD13':	17.3,
+          'TD17':	17.3,
+          'TD20':	13.5,
+          'TD21':	16.0,
+          'TD22':	11.8,
+          'TD23':	11.6,
+          'TD24':	13.9,
+          'TD25':	11.9,
+          'TD26':	10.1,
+          'TD28':	13.6,
+          'TD29':	14.2,
+          'TD30':	14.1,
+          'TD31':	11.1,
+          'TD32':	12.2,
+          'TD34':	11.6,
+          'TD35':	10.4,
+          'TD37':	12.8,
+          'TD38':	10.9,
+          'TD70':	16.3,
+          'TD77':	16.8,
+          'TD82':	13.3}
 
 
 def get_timedist_average(subjects):
@@ -50,47 +85,8 @@ def get_emg_data(subjects, newer_than=None):
     return data
 
 
-# ages for TD subjects
-age_di = {'TD01':	13.1,
-          'TD02':	12.2,
-          'TD03':	12.2,
-          'TD04':	14.3,
-          'TD05':	15.9,
-          'TD07':	11.6,
-          'TD08':	16.7,
-          'TD09':	11.6,
-          'TD10':	12.3,
-          'TD11':	13.8,
-          'TD12':	17.6,
-          'TD13':	17.3,
-          'TD17':	17.3,
-          'TD20':	13.5,
-          'TD21':	16.0,
-          'TD22':	11.8,
-          'TD23':	11.6,
-          'TD24':	13.9,
-          'TD25':	11.9,
-          'TD26':	10.1,
-          'TD28':	13.6,
-          'TD29':	14.2,
-          'TD30':	14.1,
-          'TD31':	11.1,
-          'TD32':	12.2,
-          'TD34':	11.6,
-          'TD35':	10.4,
-          'TD37':	12.8,
-          'TD38':	10.9,
-          'TD70':	16.3,
-          'TD77':	16.8,
-          'TD82':	13.3}
-age_cutoff = None
-# sessions must be newer than this
-newer_than = datetime.datetime(2018, 3, 1)
-
-
-# compute new normal data
-if __name__ == '__main__' and False:
-    logging.basicConfig(level=logging.INFO)
+def compute_model_normaldata(filename, age_cutoff=None):
+    """Compute model normaldata into plotdir/filename"""
     if age_cutoff:
         subjects = get_subjects()
         subjects = [x for x in subjects if age_di[x] < age_cutoff]
@@ -100,12 +96,12 @@ if __name__ == '__main__' and False:
     # get all data as numpy arrays
     data, nc = get_model_data(subjects)
     ndata = normaldata.normals_from_data(data)
-    fn = op.join(params['plotdir'], 'TD_normaldata_12andbelow.xlsx')
+    fn = op.join(params['plotdir'], filename)
     normaldata._write_xlsx(ndata, fn)
 
 
-if __name__ == '__main__':
-    #logging.basicConfig(level=logging.INFO)
+def compute_emg_normaldata(age_cutoff=None, newer_than=None):
+    """E.g. newer_than = datetime.datetime(2018, 3, 1)"""
     if age_cutoff:
         subjects = get_subjects()
         subjects = [x for x in subjects if age_di[x] < age_cutoff]
@@ -114,24 +110,28 @@ if __name__ == '__main__':
     logger.info('subjects: %s' % subjects)
     # get all data as numpy arrays
     data, meta = get_emg_data(subjects, newer_than=newer_than)
-    plt.figure()
-    ch = 'RSol'
-    plt.plot(data[ch].T)
-    plt.plot(np.median(data[ch], axis=0), 'k--')
-    plt.title(ch)
-
     emg_normals = dict()
     chs = (x[1:] for x in data.keys())  # strip context
     for ch_ in chs:
-        plt.figure()
         rch, lch = 'R'+ch_, 'L'+ch_
         emg_normals[ch] = np.median(data[rch], axis=0) + np.median(data[lch], axis=0)
         emg_normals[ch] /= emg_normals[ch].max()
-        plt.imshow(emg_normals[ch][None, :], aspect="auto")
-        plt.colorbar()
-        plt.title(ch_)
-        plt.axis('off')
-        
+    return emg_normals
+
+
+# compute new EMG normal data as dict of numpy arrays
+# eventual normal data will be dict of numpy arrays (1x101)
+# -write json serializer and dump to disk
+# -add cfg.emg.graded_normaldata or similar
+# -add support in plotters
+
+# 1-D heat map of EMG data
+"""
+for ch in emg_normals:
+    plt.imshow(emg_normals[ch][None, :], aspect="auto")
+    plt.colorbar()
+    plt.axis('off')
+"""
 
 # comparison with old normaldata
 """        
