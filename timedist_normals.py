b71310e7a962f438c3ef8882f44606cc17723eb7
Jussi Nurminen <jnu@iki.fi>
2021-05-14 14:53:08 +0300

    wip: timedist normals

--- timedist_normals.py
+++ timedist_normals.py
@@ -6,10 +6,11 @@ compute normal data from CP project normal subjects
 @author: Jussi (jnu@iki.fi)
 """
 # %% init
+import enum
 import json
 import logging
 
-from gaitutils.viz import plot_matplotlib, plot_misc
+from gaitutils.viz import plot_matplotlib, plot_misc, plot_plotly
 from gaitutils.envutils import _ipython_setup
 
 import cp_walk_parameters
@@ -26,14 +27,32 @@ outf = 'timedist_normals.json'
 ana_mean, ana_std = cp_walk_parameters.get_timedist_average(subjects)
 
 
+# %% maxes
 maxes = dict()
+rng = np.linspace(.25, 1, 12)
+k = 0
 for var, di in ana_mean['unknown'].items():
     val = (di['Left'] + di['Right']) / 2.0
-    maxes[var] = val
+    maxes[var] = val / rng[k]
+    k += 1
+maxes.pop('Stride Time')
 
-
-fig = plot_matplotlib.time_dist_barchart(ana_mean, stddev=ana_std)
+# %% foo2
+fig = plot_plotly.time_dist_barchart(ana_mean, stddev=ana_std, bar_scaling=maxes)
+plot_misc.show_fig(fig)
+fig = plot_matplotlib.time_dist_barchart(ana_mean, stddev=ana_std, bar_scaling=maxes)
 plot_misc.show_fig(fig)
 
-with open(outf, 'w') as f:
-    json.dump(maxes, f)
+#with open(outf, 'w') as f:
+#    json.dump(maxes, f)
+
+
+# %%
+import numpy as np
+
+a = np.array([1,2,3,4])
+
+b = np.array([10,20,30,np.nan])
+
+foo = [x if not np.isnan(x) else a[k] for k, x in enumerate(b)]
+
